0a932e9327f71589199569bebeab7350c1cb664c
Deborange <101590106+DeboraBee@users.noreply.github.com>
2025-03-31 20:19:04 -0300

    Update Aula 04.A.ac Expressões JOIN e Views.sql

--- Aula 04.A.ac Expressões JOIN e Views.sql
+++ Aula 04.A.ac Expressões JOIN e Views.sql
@@ -1,7 +1,7 @@
--- relação a partir da união das tabelas student e takes
+-- 1: relação a partir da união das tabelas student e takes
 SELECT * from student JOIN takes on student.ID = takes.ID;
 
--- contar a quantidade de cursos realizados pelos alunos do departamento de Civil Eng.
+-- 2: contar a quantidade de cursos realizados pelos alunos do departamento de Civil Eng.
 SELECT s.ID, s.name, COUNT(t.course_id) AS num_courses
 FROM student s
 LEFT JOIN takes t ON s.ID = t.ID
@@ -9,7 +9,7 @@ WHERE s.dept_name = 'Civil Eng.'
 GROUP BY s.ID, s.name
 ORDER BY num_courses DESC;
 
---Criar a view dos estudantes 
+--3: Criar a view dos estudantes 
 CREATE VIEW civil_eng_students AS
 SELECT s.ID, s.name, COUNT(t.course_id) AS num_courses
 FROM student s
